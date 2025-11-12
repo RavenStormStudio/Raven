a8ac707068a2c8f91f5d744c2ce271a93aa15179
Drischdaan <drischdaan@protonmail.com>
2025-11-12 20:05:06 +0100

    feat(raven): adds base game mode class

---
+++ Plugins/Raven/Source/Raven/Public/GameFramework/RavenGameModeBase.h
@@ -0,0 +1,13 @@
+﻿// RavenStorm Copyright @ 2025-2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RavenGameModeBase.generated.h"
+
+UCLASS()
+class RAVEN_API ARavenGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
