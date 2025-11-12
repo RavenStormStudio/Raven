90795892590b9e054871298ca0eda4b05f3a3b86
Drischdaan <drischdaan@protonmail.com>
2025-11-12 20:05:27 +0100

    feat(raven): adds base player controller class

---
+++ Plugins/Raven/Source/Raven/Public/GameFramework/RavenPlayerControllerBase.h
@@ -0,0 +1,13 @@
+﻿// RavenStorm Copyright @ 2025-2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RavenPlayerControllerBase.generated.h"
+
+UCLASS()
+class RAVEN_API ARavenPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+};
