f158fc0ff42bc4e06783b1d4bb2c2008fcc25030
Drischdaan <drischdaan@protonmail.com>
2025-11-12 19:56:50 +0100

    feat(sample): adds sample game instance class

---
+++ Source/RavenSample/GameFramework/RavenSampleGameInstance.h
@@ -0,0 +1,14 @@
+﻿// RavenStorm Copyright @ 2025-2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameFramework/RavenGameInstanceBase.h"
+#include "RavenSampleGameInstance.generated.h"
+
+UCLASS()
+class RAVENSAMPLE_API URavenSampleGameInstance : public URavenGameInstanceBase
+{
+	GENERATED_BODY()
+};
